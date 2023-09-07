9c6548768a8926a742e27da595ebfa0239399daf
sa-l-aash <salaashkaelo@gmail.com>
2023-09-07 21:38:38 +0300

    added search route

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::get('/readOneUser', [UserController::class,'readOneUser']);
 Route::get('/readAllUsers', [UserController::class,'readAllUsers']);
 Route::put('/updateUser', [UserController::class,'updateUser']);
 Route::delete('/deleteUser', [UserController::class,'deleteUser']);
+Route::get('/searchUsers', [UserController::class, 'searchUsers']);
 
 //Auth Controller routes
 Route::post('/register',[AuthController::class,'register']);
@@ -35,3 +36,4 @@ Route::post('/login',[AuthController::class,'login']);
 Route::group(['middleware'=> ['auth:sanctum']], function(){    
 Route::post('/logout',[AuthController::class,'logout']);
     });
+
